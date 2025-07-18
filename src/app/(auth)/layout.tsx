09e296afdd5d83a3c98f33f23c5bafae526ae381
abdout <osmanabdout@hotmail.com>
2025-07-18 07:56:53 +0300

    S

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,14 +1,13 @@
+"use client"
 
-const AuthLayout = ({ 
-  children
-}: { 
-  children: React.ReactNode
-}) => {
-  return ( 
-    <div className="h-screen flex items-center justify-center ">
-      {children}
+import { Suspense } from "react"
+
+const AuthLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div className="flex h-full items-center justify-center bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-sky-400 to-blue-800">
+      <Suspense>{children}</Suspense>
     </div>
-   );
+  )
 }
- 
-export default AuthLayout;
+
+export default AuthLayout
