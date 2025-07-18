13fe7ff3173372834f94957b1fdca1296d9e8f37
abdout <osmanabdout@hotmail.com>
2025-07-18 12:11:21 +0300

    j

--- src/components/providers.tsx
+++ src/components/providers.tsx
@@ -3,6 +3,7 @@
 import * as React from "react"
 import { Provider as JotaiProvider } from "jotai"
 import { ThemeProvider as NextThemesProvider } from "next-themes"
+import { SessionProvider } from "next-auth/react"
 
 import { TooltipProvider } from "@/components/ui/tooltip"
 
@@ -16,5 +17,20 @@ export function ThemeProvider({
         <TooltipProvider delayDuration={0}>{children}</TooltipProvider>
       </NextThemesProvider>
     </JotaiProvider>
+  )
+}
+
+export function Providers({ children }: { children: React.ReactNode }) {
+  return (
+    <SessionProvider>
+      <ThemeProvider
+        attribute="class"
+        defaultTheme="system"
+        enableSystem
+        disableTransitionOnChange
+      >
+        {children}
+      </ThemeProvider>
+    </SessionProvider>
   )
 }
